0a78d8748985b14876480160fbebf6adae6dda08
MOB_Xamarin <MOB_Xamarin>
2016-10-24 22:24:30 -0700

    Adicionando o projeto MVVM

---
+++ MVVMPattern/MVVMPattern/MVVMPattern/App.cs
@@ -0,0 +1,35 @@
+﻿using MVVMPattern.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MVVMPattern
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new AlunoViewPage();
+            
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MVVMPattern/MVVMPattern/MVVMPattern/View/AlunoViewPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using MVVMPattern.Model;
+using MVVMPattern.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MVVMPattern.View
+{
+    public partial class AlunoViewPage : ContentPage
+    {
+        AlunoViewModel vmAluno;
+
+        public AlunoViewPage()
+        {
+            //Aluno aluno = AlunoViewModel.GetAluno();
+            //vmAluno = new AlunoViewModel(aluno);
+            //BindingContext = vmAluno;
+
+            vmAluno = new AlunoViewModel();
+            BindingContext = vmAluno;
+
+            
+            InitializeComponent();
+        }
+
+        private void OnItemTapped(object sender, ItemTappedEventArgs args)
+        {
+            var alunoSelecionado = args.Item as Aluno;
+            DisplayAlert("Aluno Selecionado",string.Format("ID do Auno :{0}", alunoSelecionado.Id),"OK");
+        }
+    }
+}
