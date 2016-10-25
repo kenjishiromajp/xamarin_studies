36b1843587ad538de70cfd551bd71a0d73500af0
MOB_Xamarin <MOB_Xamarin>
2016-10-24 20:54:21 -0700

    TabbedPages atualizado

---
+++ MOB_XF_Hello/MOB_XF_Hello/Views/TabPage1.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MOB_XF_Hello.Views
+{
+    public partial class TabPage1 : ContentPage
+    {
+        public TabPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MOB_XF_Hello/MOB_XF_Hello/Views/TabPage3.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MOB_XF_Hello.Views
+{
+    public partial class TabPage3 : ContentPage
+    {
+        public TabPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
