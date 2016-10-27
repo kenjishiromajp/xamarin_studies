81642a788f24e8540a5ca0b82f2aa69488175ca0
MOB_Xamarin <MOB_Xamarin>
2016-10-26 17:44:29 -0700

    MVVM Agrupamento

---
+++ MVVMPattern/MVVMPattern/MVVMPattern/Code/Agrupamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMPattern.Code
+{
+    class Agrupamento<K,T> : ObservableCollection<T>
+    {
+        public K Chave { get; private set; }
+        public Agrupamento(K chave, IEnumerable<T> items)
+        {
+            Chave = chave;
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+}
--- MVVMPattern/MVVMPattern/MVVMPattern/Model/Aluno.cs
+++ MVVMPattern/MVVMPattern/MVVMPattern/Model/Aluno.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +13,29 @@ namespace MVVMPattern.Model
         public Guid Id { get; set; }
         public string RM { get; set; }
         public string Nome { get; set; }
-        public string Email { get; set; }
+
+        private string email { get; set; }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Email)));
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
+        //private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        //{
+        //    if(PropertyChanged != null)
+        //    {
+        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
     }
 
 
